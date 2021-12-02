6196fdf4d49d49417a30f593dd66347570f88166
Dima <34dd@34dd.ru>
2021-12-02 11:34:04 +0300

    02.12.2021

---
+++ Java_AutoTest_WebUI/Lesson_02/src/main/java/org/example/Lesson_02.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/*
+Lesson_08 (GB.Java Core)
+Denisenko Dmitriy (02.12.2021)
+ */
+public class Lesson_02
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

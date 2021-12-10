e4d031d8a7666ea3c02555d3c17db62c60286626
Dima <34dd@34dd.ru>
2021-12-10 17:22:54 +0300

    10.12.2021

---
+++ Java_AutoTest_WebUI/Lesson_04/src/main/java/Lesson_04/Lesson_04.java
@@ -0,0 +1,12 @@
+package Lesson_04;
+/*
+Lesson_04 (GB.Java_AutoTest_WebUI)
+Denisenko Dmitriy (02.12.2021)
+ */
+public class Lesson_04
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Java_AutoTest_WebUI/Lesson_04/src/main/java/Lesson_04/My_isTriangleException.java
@@ -0,0 +1,5 @@
+package Lesson_04;
+
+public class My_is_TriangleException extends Exception {
+
+}

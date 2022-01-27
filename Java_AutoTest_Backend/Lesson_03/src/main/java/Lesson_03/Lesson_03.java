b15ed0a134866db3fc4c6f3154189a571ebc753f
Dima <34dd@34dd.ru>
2022-01-27 16:44:03 +0300

    27.01.2022

---
+++ Java_AutoTest_Backend/Lesson_03/src/main/java/Lesson_03/Lesson_03.java
@@ -0,0 +1,13 @@
+package Lesson_03;
+
+/*
+Lesson_03 (GB.Java_AutoTest_Backend)
+Denisenko Dmitriy (25.01.2022)
+ */
+public class Lesson_03
+{
+    public static void main(String... args )
+    {
+
+    }
+}
